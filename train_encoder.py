0fb31b43ef27844d007bd33bf855405d20e9f980
Allen <39018805+wangcxallen@users.noreply.github.com>
2020-04-22 22:19:03 -0400

    update epoch num 5000 embeding 25

--- train_encoder.py
+++ train_encoder.py
@@ -12,7 +12,7 @@ def main():
     train_data = 'data/' + 'tol_data_1000.npy'
     test_data = 'data/' + 'tol_data_50.npy'
     ae = tol_encoder(train_data, test_data)
-    ae.train(num_epoch=1)
+    ae.train(num_epoch=5000)
     ae.save_weights('ae_weights')
     
     pass
